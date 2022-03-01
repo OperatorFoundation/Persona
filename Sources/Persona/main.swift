cea25f335a96446080e1613fb63591fe7f27941e
CryptoSax <joshua@operatorfoundation.org>
2022-03-01 09:26:16 -0600

    added comments

--- Sources/Persona/main.swift
+++ Sources/Persona/main.swift
@@ -3,9 +3,12 @@ import Foundation
 import Spacetime
 import Simulation
 
+// run in one XCode window while you run the flower test in another
 func main()
 {
     let simulation = Simulation(capabilities: Capabilities(display: true, networkConnect: true, networkListen: true))
+    // effects are outputs
+    // events are inputs
     let universe = Persona(effects: simulation.effects, events: simulation.events)
 
     do
