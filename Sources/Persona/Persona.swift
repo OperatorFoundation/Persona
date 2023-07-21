fcc7f1300b5534843b6258f02e28260d37c6a3bc
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2023-07-21 17:51:30 -0500

    Improved error logging

--- Sources/Persona/Persona.swift
+++ Sources/Persona/Persona.swift
@@ -66,7 +66,8 @@ public class Persona
             }
             catch
             {
-                continue
+                self.logger.error("\(error.localizedDescription)")
+                return
             }
         }
     }
