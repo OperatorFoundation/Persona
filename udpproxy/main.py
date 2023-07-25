b31f80012d5d0befc8eb011c486dc9b81d0342b1
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2023-07-24 20:04:59 -0500

    logging

--- udpproxy/main.py
+++ udpproxy/main.py
@@ -78,6 +78,10 @@ class UdpProxy:
 
                 length = len(data) + 6
                 lengthBytes = length.to_bytes(4, "big")
+
+                self.log.write("total length %d" % (length))
+                self.log.flush()
+
                 parts = host.split(".")
                 hostBytes = chr(parts[0]) + chr(parts[1]) + chr(parts[2]) + chr(parts[3])
                 portBytes = port.to_bytes("big")
