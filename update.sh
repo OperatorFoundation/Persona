34b8b3184da4cec04920d0b0ec3b7f728d58133a
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2023-08-09 17:22:14 -0500

    kill existing connections in update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,16 @@ git pull origin main
 swift package update
 swift build
 
-systemctl restart persona.socket
-systemctl restart udpproxy.socket
-systemctl restart tcpproxy.socket
+systemctl stop persona.socket
+systemctl stop udpproxy.socket
+systemctl stop tcpproxy.socket
 
+killall main.py
+killall -9 main.py
+
+killall Persona
+killall 9 Persona
+
+systemctl start tcpproxy.socket
+systemctl start udpproxy.socket
+systemctl start persona.socket
