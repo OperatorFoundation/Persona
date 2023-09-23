98ba4a0eb372f4947918b434d9d1fce1eaa55c5b
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2023-09-23 15:58:52 -0500

    Move frontend in front and Persona out of the way

--- install.sh
+++ install.sh
@@ -17,8 +17,8 @@ cp etc/xinetd/* /etc/xinetd.d
 systemctl restart xinetd
 
 ufw allow 22   # ssh
-ufw allow 1230 # frontend
-ufw allow 1234 # Persona
+ufw allow 1234 # frontend
+ufw allow 1230 # Persona
 ufw deny 7     # echo
 ufw deny 1233  # tcpproxy
 ufw deny 1232  # udpproxy
