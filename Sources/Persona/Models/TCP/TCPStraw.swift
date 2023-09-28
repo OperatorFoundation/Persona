f1e0792b5939c0547a9a6306a4d84f0ac7757bb7
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2023-09-28 01:11:32 -0500

    logging

--- Sources/Persona/Models/TCP/TCPStraw.swift
+++ Sources/Persona/Models/TCP/TCPStraw.swift
@@ -134,11 +134,11 @@ public class TCPStraw
 
     public func read(window: SequenceNumberRange) throws -> SegmentData
     {
-        print("TCPStraw.read - 1")
+        self.logger.info("TCPStraw.read - 1")
         let offset = window.lowerBound - self.sequenceNumber
-        print("TCPStraw.read - 2")
+        self.logger.info("TCPStraw.read - 2")
         let size = window.upperBound - window.lowerBound
-        print("TCPStraw.read - 3")
+        self.logger.info("TCPStraw.read - 3")
         return try self.read(offset: Int(offset), size: Int(size))
     }
 
