1e8d8420b5a99bbb1bcec7840135a8fdac47265c
consuelita <adelita@operatorfoundation.org>
2024-02-18 13:05:52 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         .package(url: "https://github.com/OperatorFoundation/Straw", from: "1.0.1"),
         .package(url: "https://github.com/OperatorFoundation/swift-log-file", from: "0.1.0"),
         .package(url: "https://github.com/OperatorFoundation/SwiftHexTools", from: "1.2.6"),
-        .package(url: "https://github.com/OperatorFoundation/TransmissionAsync", from: "0.1.1"),
+        .package(url: "https://github.com/OperatorFoundation/TransmissionAsync", from: "0.1.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
