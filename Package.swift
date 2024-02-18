1f85a6641a1de36c3b7ae154c87d89cd0ee22ad4
consuelita <adelita@operatorfoundation.org>
2024-02-18 12:34:31 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
         
         .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.1"),
         .package(url: "https://github.com/OperatorFoundation/Gardener", from: "0.1.1"),
-        .package(url: "https://github.com/OperatorFoundation/InternetProtocols", from: "2.2.2"),
-        .package(url: "https://github.com/OperatorFoundation/Net", from: "0.0.9"),
+        .package(url: "https://github.com/OperatorFoundation/InternetProtocols", from: "2.2.3"),
+        .package(url: "https://github.com/OperatorFoundation/Net", from: "0.0.10"),
         .package(url: "https://github.com/OperatorFoundation/Straw", from: "1.0.1"),
         .package(url: "https://github.com/OperatorFoundation/swift-log-file", from: "0.1.0"),
         .package(url: "https://github.com/OperatorFoundation/SwiftHexTools", from: "1.2.6"),
