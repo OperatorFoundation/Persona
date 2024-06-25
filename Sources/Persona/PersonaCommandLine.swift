cea2708618b3e68a40151c2ab2d79dd7955b096c
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2024-06-25 14:56:46 -0500

    Resolved a FIXME to migrate from ParseableCommand to AsyncParseableCommand

--- Sources/Persona/PersonaCommandLine.swift
+++ Sources/Persona/PersonaCommandLine.swift
@@ -15,31 +15,21 @@ import FoundationNetworking
 // The main entry point for Persona uses ArgumentParser to create a command line interface.
 // It only supports one flag, -socket, which is used to run Persona without the router, only
 // for testing purposes. In production deployment, it is intended to be run by the router.
-struct PersonaCommandLine: ParsableCommand
+@main
+struct PersonaCommandLine: AsyncParsableCommand
 {
     @Flag // Enable single connection socket-based mode, designed for testing
     var socket: Bool = false // By default, use router mode
 
-    mutating func run() throws
+    mutating func run() async throws
     {
         print("Persona is go!")
 
         let socketOption = socket
 
-        // FIXME - migrate to AsyncParsableCommand to remove this lock.
-        let lock: DispatchSemaphore = DispatchSemaphore(value: 0)
-
-        Task
-        {
-            let persona = try await Persona(socket: socketOption)
-            try await persona.run()
-            lock.signal()
-        }
-
-        lock.wait()
+        let persona = try await Persona(socket: socketOption)
+        try await persona.run()
 
         print("exiting abnormally, something forgot to wait")
     }
 }
-
-PersonaCommandLine.main()
