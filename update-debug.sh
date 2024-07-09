6b6e57ed2177d70f0fa783576a72b09d906b2c17
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2024-07-09 13:48:24 -0500

    Debugging-friendly update.sh

---
+++ update-debug.sh
@@ -0,0 +1,40 @@
+git stash
+git pull
+
+swift package reset
+rm -rf .build >/dev/null
+rm -rf .swiftpm >/dev/null
+rm -rf Package.resolved >/dev/null
+
+swift package update
+swift build
+cp .build/x86_64-unknown-linux-gnu/debug/Persona . >/dev/null 2>/dev/null
+cp .build/arm64-apple-macosx/debug/Persona . >/dev/null 2>/dev/null
+
+apt install golang
+pushd frontend
+go version
+go mod init frontend
+go mod tidy
+go get frontend
+go install
+popd
+pushd router
+go mod init router
+go mod tidy
+go get router
+go install
+popd
+
+systemctl stop frontend
+
+killall Persona >/dev/null 2>/dev/null
+killall -9 Persona >/dev/null 2>/dev/null
+
+killall router >/dev/null 2>/dev/null
+killall -9 router >/dev/null 2>/dev/null
+
+killall frontend >/dev/null 2>/dev/null
+killall -9 frontend >/dev/null 2>/dev/null
+
+systemctl start frontend
